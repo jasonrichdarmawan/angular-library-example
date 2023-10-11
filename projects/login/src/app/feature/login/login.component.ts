4cbcdb89b5659637d9e3e60beaa44d2c70b59465
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-10-11 11:53:55 +0700

    chore: move some items as a library
    1. shared/auth as the @@auth library
    2. shared/login as the @@login application
    3. shared/models as the @@models library

--- projects/login/src/app/feature/login/login.component.ts
+++ projects/login/src/app/feature/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from 'src/app/models/user';
-import { AuthService } from 'src/app/shared/auth/auth.service';
+import { User } from '@@models';
+import { AuthService } from '@@auth';
 
 @Component({
   selector: 'app-login',
